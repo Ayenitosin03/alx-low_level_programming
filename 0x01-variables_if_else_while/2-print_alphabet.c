d54c4c2b9aca50d9e45147965315236f40476c4d
Oluwatosin Ayeni <53630049+Ayenitosin03@users.noreply.github.com>
2022-03-11 01:33:01 -0800

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-        char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
+	char ch;
 
-	        putchar('\n');
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+		putchar('\n');
 
-	return (0);
+return (0);
 }
