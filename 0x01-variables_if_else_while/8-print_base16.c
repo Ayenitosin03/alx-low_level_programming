457e99f1cff01e0d1e84d883eca87de336f086f5
Oluwatosin Ayeni <53630049+Ayenitosin03@users.noreply.github.com>
2022-03-12 06:00:12 -0800

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,41 +1,23 @@
 #include <stdio.h>
 
-
-
 /**
- *
- * * main - Print the numbers of base 16 in lowercase
- *
- * *
- *
- * * Return: Always 0 (Success)
- *
- * */
-
+ * main - prints all the numbers of base 16 in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+	int n;
+	char ch;
 
-			char digit;
-
-
-
-					for (digit = '0'; digit <= '9'; digit++)
-
-								putchar(digit);
-
-
-
-							for (digit = 'a'; digit <= 'f'; digit++)
-
-										putchar(digit);
-
-
-
-									putchar('\n');
-
-
-
-										return (0);
-
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
